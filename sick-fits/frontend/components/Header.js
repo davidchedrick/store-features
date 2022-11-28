e721a69c2ee865a61d920eabca2e044e725d33a5
David Chedrick <davidchedrick@gmail.com>
2022-11-28 11:21:20 -0600

    added styled componets

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,11 +1,29 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">Bodhi Cats!!!</Link>
+        <Logo>
+          <Link href="/">Bodhi Cat!!!</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
