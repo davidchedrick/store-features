2068168e9b2acb3708943418db9ec2733951bce7
David Chedrick <davidchedrick@gmail.com>
2023-03-21 14:01:39 -0500

    added password reset

---
+++ sick-fits/frontend/components/SignUp.js
@@ -0,0 +1,94 @@
+/* eslint-disable react/jsx-no-bind */
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import Error from './ErrorMessage';
+
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+export default function SignUp() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    name: '',
+    password: '',
+  });
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+    variables: inputs,
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log('inputs:', inputs);
+    const res = await signup().catch(console.error);
+    console.log('res', res);
+    console.log('{ data, loading, error }: ', { data, loading, error });
+
+    resetForm();
+    // Send the email and password to the graphqlAPI
+  }
+
+  // const error =
+  //   data?.authenticateUserWithPassword.__typename ===
+  //   'UserAuthenticationWithPasswordFailure'
+  //     ? data?.authenticateUserWithPassword
+  //     : undefined;
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Up For An Account</h2>
+      <Error error={error} />
+      <fieldset>
+        {data?.createUser && (
+          <p>Signed up with {data.createUser.email} - Please Sign in!</p>
+        )}
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+}
